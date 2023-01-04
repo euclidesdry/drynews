0ee0f132fd84aa37bdd1684529de4cdc86be31e7
Euclides do Rosário <euclidesdry@gmail.com>
2023-01-04 14:34:28 +0100

    feat(styles): 💄 adding global styles file

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import styles from "../styles/home.module.scss";
 
 export default function Home() {
   return (
@@ -7,7 +6,7 @@ export default function Home() {
       <Head>
         <title>Home | DRY.news</title>
       </Head>
-      <h1 className={styles.title}>DRY Products</h1>
+      <h1>DRY Products</h1>
     </>
   );
 }
