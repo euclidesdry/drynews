7852f93d4e1ba16a4a6da111e656186f5fe2c4c9
Euclides do Rosário <euclidesdry@gmail.com>
2023-01-04 14:20:25 +0100

    feat(project): ✨ setting external font-family

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,13 @@
+import Head from "next/head";
 import styles from "../styles/home.module.scss";
 
 export default function Home() {
-  return <h1 className={styles.title}>DRY Products</h1>;
+  return (
+    <>
+      <Head>
+        <title>Início | DRY.news</title>
+      </Head>
+      <h1 className={styles.title}>DRY Products</h1>
+    </>
+  );
 }
