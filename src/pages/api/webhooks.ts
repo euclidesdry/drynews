8ff1be590281b10010fb30cef0157f52a352436a
Euclides do Rosário <euclidesdry@gmail.com>
2023-01-10 20:03:01 +0100

    feat(payments): ✨ stripe Webhooks

---
+++ src/pages/api/webhooks.ts
@@ -0,0 +1,8 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default (req: NextApiRequest, res: NextApiResponse) => {
+    console.log("evento recebido");
+
+    res.status(200).json({ ok: true });
+};
