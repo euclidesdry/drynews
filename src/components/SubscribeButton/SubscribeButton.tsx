13a8f889d78cc1379f6105ed32d67f6af4617bff
Euclides do Rosário <euclidesdry@gmail.com>
2023-01-10 16:55:40 +0100

    feat(payments): ✨ redirecting to stripe checkout

--- src/components/SubscribeButton/SubscribeButton.tsx
+++ src/components/SubscribeButton/SubscribeButton.tsx
@@ -2,6 +2,8 @@ import { useSession, signIn } from "next-auth/react";
 
 import styles from "./styles.module.scss";
 import { ProductProps } from "../../pages";
+import { api } from "../../services/api";
+import { getStripeJs } from "../../services/stripe-js";
 
 interface SubscribeButtonProps {
     priceId: ProductProps["priceId"];
@@ -10,13 +12,24 @@ interface SubscribeButtonProps {
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
     const { data, status } = useSession();
 
-    function handleSubscribe() {
+    async function handleSubscribe() {
         if (status === "unauthenticated") {
             signIn("github");
             return;
         }
 
-        // criação da checkout session
+        try {
+            const response = await api.post("/subscribe");
+
+            const { sessionId } = response.data;
+
+            const stripe = await getStripeJs();
+
+            await stripe?.redirectToCheckout({ sessionId });
+        } catch (err) {
+            const error = err as Error;
+            alert(error.message);
+        }
     }
 
     return (
