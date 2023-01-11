25701e75c8e1f821c058e3b27386ba289f438b23
Euclides do Rosário <euclidesdry@gmail.com>
2023-01-11 21:33:59 +0100

    feat(project): ✨ menu navigation

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,29 +3,32 @@ import { ReactNode } from "react";
 import styles from "./styles.module.scss";
 import { SigInButton } from "../SignInButton/SigInButton";
 import Image from "next/image";
+import Link from "next/link";
 
 interface HeaderProps {}
 
 export function Header({}: HeaderProps) {
-  return (
-    <header className={styles.headerContainer}>
-      <div className={styles.headerContent}>
-        <Image
-          src="/images/logo.svg"
-          alt="DRY.news Logo"
-          width={120}
-          height={27}
-        />
+    return (
+        <header className={styles.headerContainer}>
+            <div className={styles.headerContent}>
+                <Image
+                    src="/images/logo.svg"
+                    alt="DRY.news Logo"
+                    width={120}
+                    height={27}
+                />
 
-        <nav>
-          <a className={styles.active} href="#">
-            Home
-          </a>
-          <a href="#">Articles</a>
-        </nav>
+                <nav>
+                    <Link href="/" className={styles.active}>
+                        Home
+                    </Link>
+                    <Link href="/posts" prefetch>
+                        Articles
+                    </Link>
+                </nav>
 
-        <SigInButton />
-      </div>
-    </header>
-  );
+                <SigInButton />
+            </div>
+        </header>
+    );
 }
