b0c157f75f19f8395d02fcc290ea9ee82f81070f
Euclides do Rosário <euclidesdry@gmail.com>
2023-01-11 19:04:01 +0100

    feat(posts): ✨ configuring Prismic CMS

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,24 @@
 import type { AppProps } from "next/app";
 import { Header } from "../components/Header";
 import { SessionProvider } from "next-auth/react";
+import Link from "next/link";
+import { PrismicProvider } from "@prismicio/react";
+import { PrismicPreview } from "@prismicio/next";
+import { repositoryName } from "../../prismicio";
 
 import "../styles/global.scss";
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
         <SessionProvider>
-            <Header />
-            <Component {...pageProps} />
+            <PrismicProvider
+                internalLinkComponent={(props) => <Link {...props} />}
+            >
+                <PrismicPreview repositoryName={repositoryName}>
+                    <Header />
+                    <Component {...pageProps} />
+                </PrismicPreview>
+            </PrismicProvider>
         </SessionProvider>
     );
 }
-
-
